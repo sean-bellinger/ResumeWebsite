ed8bb580bc51a5488053bf89dc56b6c1990355a7
Sean <onething.sean@gmail.com>
2022-07-22 10:30:12 -0700

    Added info to the home page, and socials.

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,26 @@
-import Navbar from "./navbar";
-import AboutMe from "./pages/aboutMe"
-import ProjectNotes from "./pages/projectNotes"
+// import Navbar from "./navbar";
+// import AboutMe from "./pages/aboutMe"
+// import ProjectNotes from "./pages/projectNotes"
 import Home from "./pages/home"
 
 function App() {
-  let currentpage;
-  switch (window.location.pathname) {
-    case "/":
-      currentpage = <Home />
-      break
-    case "/projectNotes":
-      currentpage = <ProjectNotes/>
-      break
-    case "/aboutMe":
-      currentpage = <AboutMe/>
-      break
-  }
+  // let currentpage;
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     currentpage = <Home />
+  //     break
+  //   case "/projectNotes":
+  //     currentpage = <ProjectNotes/>
+  //     break
+  //   case "/aboutMe":
+  //     currentpage = <AboutMe/>
+  //     break
+  // }
   return (
     <>
-      <Navbar />
-      {currentpage}
+      <Home />
+      {/* <Navbar />
+      {currentpage} */}
     </>
   )
 }
--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,5 +1,18 @@
 export default function Home() {
     return <div>
-        Welcome to my page.
+        <a class="social-container" href="https://www.linkedin.com/in/sean-bellinger/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
+        <a class="social-container" href="https://github.com/sean-bellinger?tab=repositories" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+        <div>
+            Email: sean.eric.bellinger@gmail.com <br></br>
+            Phone: 564.203.8727 <br></br>
+            Location: Vancouver, Washington
+        </div>
+        <div>
+            Software Engineer with a focus on Full Stack JavaScript development, primarily with React and Node.js. <br></br>
+
+            Strong: JavaScript (All ECMAScripts) | React | Webpack | HTML5 | CSS | Node.js | SQL | Git | Linux <br></br>
+
+            Experienced: Kubernetes | Jira | Docker | Terraform | AWS Cloud Computing
+        </div>
     </div>
 }
