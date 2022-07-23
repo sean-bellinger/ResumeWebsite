dec777ba5c0b41e56ffa85b2d303e102687d77d6
Sean <onething.sean@gmail.com>
2022-07-23 14:48:34 -0700

    Reduced unnecessary code.

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,18 +1,18 @@
 export default function Home() {
     return <div>
-        <a class="social-container" href="https://www.linkedin.com/in/sean-bellinger/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
-        <a class="social-container" href="https://github.com/sean-bellinger?tab=repositories" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+        <h1>HELLO WORLD</h1>
         <div class="contact-info">
-            Email: sean.eric.bellinger@gmail.com <br></br>
-            Phone: 564.203.8727 <br></br>
-            Location: Vancouver, Washington
+            Email: sean.eric.bellinger@gmail.com <br/>
+            Location: Greater Portland, Oregon area.
         </div>
         <div>
-            Software Engineer with a focus on Full Stack JavaScript development, primarily with React and Node.js. <br></br>
+            Software Engineer with a focus on Full Stack JavaScript development, primarily with React and Node.js. <br/>
 
-            Strong: JavaScript (All ECMAScripts) | React | Webpack | HTML5 | CSS | Node.js | SQL | Git | Linux <br></br>
+            Strong: JavaScript (All ECMAScripts) | React | Webpack | HTML5 | CSS | Node.js | SQL | Git | Linux <br/>
 
-            Experienced: Kubernetes | Jira | Docker | Terraform | AWS Cloud Computing
+            Actively Learning: Kubernetes | Jira | Docker | Terraform | AWS Cloud Computing | Typescript
         </div>
+        <a class="social-container" href="https://www.linkedin.com/in/sean-bellinger/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
+        <a class="social-container" href="https://github.com/sean-bellinger?tab=repositories" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
     </div>
 }
