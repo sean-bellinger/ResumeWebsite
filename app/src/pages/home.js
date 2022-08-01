5912ee5e9d91dacbca9f7cc90a10620595d49e47
Sean <onething.sean@gmail.com>
2022-08-01 15:37:26 -0700

    Added dynamic greeting, clearer text about interests and myself, and a few style changes.

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -45,44 +45,73 @@ function Item(props) {
       />
     );
   }
+  var greeting = function(){
+    let currentHour = new Date().getHours()
+    if(currentHour > 4 && currentHour < 12){
+      return 'Good Morning'
+    }
+    if(currentHour < 18){
+      return 'Good Afternoon'
+    }
+    if(currentHour < 21){
+      return 'Good Evening'
+    }
+      return 'Good Night'
+  }
 
 export default function Home() {
     return <div>
-        <Container maxWidth="sm">
-            <Box sx={{ spacing: "mr" }}>
-                <Typography variant="h6">
-                    Email: sean.eric.bellinger@gmail.com <br/>
-                    Location: Greater Portland, Oregon area.
-                </Typography>
-            </Box>
-            <Box>    
-                    Software Engineer with a focus on Full Stack JavaScript development, primarily with React and Node.js.
-            </Box>
+        <Container>
+            <Card>
+              <Typography sx={{ textAlign: 'center' }} variant='h4'>
+                {greeting()} World, I'm Sean Bellinger!
+              </Typography>
+              <Typography sx={{ textAlign: 'center' }} variant='body1'>
+                I'm an aspiring full stack developer located in the greater Portland OR area. I graduated from Hack Reactor, now known as Galvanize, in 2018,
+                and I am relaunching my efforts to enter into a full stack developer career.<br/><br/>
+
+                Job opportunies that would be intriguing to me are ones where I can further my learning in Javascript and/or Typescript and have the opportunity
+                to explore CI/CD and Cloud infrastructure.<br/><br/>
+
+                Many of my friends and employers would say I'm a very likable guy. I have overall strong communication skills, view teamwork as enjoyable,
+                and find sovling puzzles to be something I seek in my free time.<br/><br/>
+
+                This website was built with React, Docker, Terraform, and deployed on Digital Ocean. I invite you all to check out my source code
+                <a href="https://github.com/sean-bellinger/ResumeWebsite" target="_blank">here</a>. <br/><br/>
+              </Typography>
+            </Card>
             <Box sx={{ display: 'flex', flexWrap: 'nowrap' }}>
                 <Card sx={{ display: 'flex', border: 1, borderRadius: '12px', flexWrap: 'wrap'}} >
-                    <Item sx={{ border: 0 }} >Strong:</Item>
-                    <Item > JavaScript (All ECMAScripts)</Item>
+                    <Item sx={{ border: 0 }} >Comfortable with:</Item>
+                    <Item > JavaScript</Item>
                     <Item >React</Item>
-                    <Item >Webpack</Item>
                     <Item >HTML5</Item>
                     <Item >CSS</Item>
                     <Item >Node.js</Item>
-                    <Item >SQL</Item>
                     <Item >Git</Item>
                     <Item >Linux</Item>
                 </Card>
                 <Card sx={{ display: 'flex', border: 1, borderRadius: '12px', flexWrap: 'wrap'}}>
                     <Item sx={{ border: 0 }} >Actively Learning:</Item>
-                    <Item>Kubernetes</Item>
                     <Item>Jira</Item>
                     <Item>Docker </Item>
                     <Item>Terraform </Item>
-                    <Item>AWS Cloud Computing </Item> 
                     <Item>Typescript </Item>
                 </Card>
-            </Box>    
-            <a class="social-container" href="https://www.linkedin.com/in/sean-bellinger/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
-            <a class="social-container" href="https://github.com/sean-bellinger?tab=repositories" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+                <Card sx={{ display: 'flex', border: 1, borderRadius: '12px', flexWrap: 'wrap'}}>
+                    <Item sx={{ border: 0 }} >Interested in:</Item>
+                    <Item>Kubernetes</Item>
+                    <Item>AWS Cloud Computing </Item> 
+
+                </Card>
+            </Box>
+            <Typography variant="h6">
+                  Email: sean.eric.bellinger@gmail.com <br/>
+            </Typography>
+              <Typography variant="h3">
+                <a class="social-container" href="https://www.linkedin.com/in/sean-bellinger/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
+                <a class="social-container" href="https://github.com/sean-bellinger?tab=repositories" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+              </Typography>
         </Container>
     </div>
 }
